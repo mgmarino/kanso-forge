d782cd7da004d586417aa8188e78598da1d93347
Michael Marino <mmarino@gmail.com>
2014-10-24 17:52:54 +0200

    Include web worker scripts

--- generate_with_version.sh
+++ generate_with_version.sh
@@ -4,10 +4,13 @@ VERSION="0.6.14"
 URL="http:\/\/github.com\/digitalbazaar\/forge"
 INSTFOLDER=node_modules/node-forge
 rm -rf node_modules
+rm -rf $MODULENAME
 npm install node-forge@$VERSION 
 pushd $INSTFOLDER  && npm install && npm run bundle && popd
 cp $INSTFOLDER/js/forge.bundle.js forge.js
 patch -p1 < forge.patch
+mkdir $MODULENAME
+cp $INSTFOLDER/js/prime.worker.js $INSTFOLDER/js/jsbn.js $MODULENAME
 for var in README.md kanso.json
 do
   sed -e s/@VERSION@/$VERSION/g \
